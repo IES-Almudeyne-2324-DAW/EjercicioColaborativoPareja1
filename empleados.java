7a5fc9bfbf69bf9038bdc812b6d274499f5d2176
alopvaz <alopvaz014@iesalmudeyne.es>
2023-10-24 14:13:35 +0200

    Subir archivo bien

---
+++ empleados.java
@@ -0,0 +1,10 @@
+public class Empleado {
+   
+    private String nif;
+    private String nombre;
+    private double sueldoBase;
+    private int horasExtras;
+    private char casado; 
+    private int numeroHijos;
+
+}
