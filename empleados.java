e316f696448dde533a803fbf715153b3978252c8
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-10-27 10:33:01 +0200

    Introducidos los comentarios JavaDoc

--- empleados.java
+++ empleados.java
@@ -1,4 +1,8 @@
-//Clase Empleado
+/**
+ * Clase empleados
+ * 
+ * @author Tibu Mayo y Alicia Lopez
+ */
 public class Empleado {
    
     private String nif;
@@ -12,63 +16,121 @@ public class Empleado {
     public Empleado() {
     }
 
-    //Constructor con dos parámetros
+    /**
+	   * Constructor con dos parametros
+	   * 
+	   * @param nif
+	   * @param nombre
+	   */
     public Empleado(String nif, String nombre) {
         this.nif = nif;
         this.nombre = nombre;
     }
 
     //Métodos set de todos los atributos
+
+    /**
+	   * 
+	   * @param casado un caracter S o N
+	   */
     public void setCasado(char casado) {
         this.casado = casado;
     }
 
+     /**
+	   * 
+	   * @param horasExtras
+	   */
     public void setHorasExtras(int horasExtras) {
         this.horasExtras = horasExtras;
     }
 
+    /**
+	   * 
+	   * @param nif
+	   */
     public void setNif(String nif) {
         this.nif = nif;
     }
 
+    /**
+	   * 
+	   * @param nombre
+	   */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+	   * 
+	   * @param numeroHijos
+	   */
     public void setNumeroHijos(int numeroHijos) {
         this.numeroHijos = numeroHijos;
     }
 
+    /**
+	   * 
+	   * @param sueldoBase
+	   */
     public void setSueldoBase(double sueldoBase) {
         this.sueldoBase = sueldoBase;
     }
 
-    //Metodo calcular el importe de las horas extra de los empleados
+    /**
+	   * Metodo calcular el importe de las horas extra de los empleados
+	   * 
+	   * @return horasExtras
+	   */
     public double calcularImporteHorasExtras(){
         return horasExtras * pagoPorHoraExtra;
     }
 
     //metodos get de todos los atributos
+    /**
+	   * 
+	   * @return S o N, si esta casado o no.
+	   */
     public char getCasado() {
         return casado;
     }
 
+    /**
+	   * 
+	   * @return int con las horas extras
+	   */
     public int getHorasExtras() {
         return horasExtras;
     }
 
+    /**
+	   * 
+	   * @return String con el dni del empleado
+	   */
      public String getNif() {
         return nif;
     }
 
+    /**
+	    * 
+	    * @return String con el nombre del empleado
+	    */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+	   * 
+	   * @return int con el numero de hijos del empleado
+	   */
     public int getNumeroHijos() {
             return numeroHijos;
         }
 
+    /**
+	   * 
+	   * @return double con el sueldo base del empleado
+	   */
     public double getSueldoBase() {
         return sueldoBase;
     }
